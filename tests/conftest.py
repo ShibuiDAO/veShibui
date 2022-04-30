76bd8a465c2170644342dff40fdef2ee04760d96
Nejc Drobnic <ethoarx@gmail.com>
2022-04-30 22:51:38 +0200

    feat: test rewards gauge

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+import logging
 import pytest
+from brownie_tokens import ERC20
 
 
 def approx(a, b, precision=1e-10):
@@ -17,3 +19,23 @@ def voting_escrow(VotingEscrow, accounts, token):
     yield VotingEscrow.deploy(
         token, "Voting-escrowed SHIBUI", "veSHIBUI", {"from": accounts[0]}
     )
+
+
+@pytest.fixture(scope="module")
+def coin_reward():
+    yield ERC20("Boba WAGMI v3 Option", "WAGMIv3", 18)
+
+
+@pytest.fixture(scope="module")
+def rewards_only_gauge(RewardsOnlyGauge, accounts, mock_lp_token):
+    yield RewardsOnlyGauge.deploy(accounts[0], mock_lp_token, {"from": accounts[0]})
+
+
+@pytest.fixture(scope="module")
+def mock_lp_token(ERC20LP, accounts):
+    yield ERC20LP.deploy("Oolong LP token", "OLP", 18, 10 ** 9, {"from": accounts[0]})
+
+
+@pytest.fixture(scope="module")
+def logger():
+    yield logging.getLogger('Shibui')
